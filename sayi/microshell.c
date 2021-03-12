3875e7d00f0a4f0ca974fbb45ec1619ae333c737
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-03-12 18:46:21 +0900

    feat: Add sayi ft_nonbuilt

--- sayi/microshell.c
+++ sayi/microshell.c
@@ -70,6 +70,50 @@ void arg_pars(int ac, char **av, t_cmd *cmd) {
 	}
 }
 
+int ft_cd(t_cmd *cmd) {
+	int i = 0;
+	int res = 0;
+
+	while (cmd->args[i])
+		i++;
+	if (i != 2) {
+		ft_putstr("error: cd: bad arguments\n");
+		return (1);
+	}
+	else if ((res = chdir(cmd->args[1])) < 0) {
+		ft_putstr("error: cd: cannot change directory to ");
+		ft_putstr(cmd->args[1]);
+		ft_putstr("\n");
+	}
+	return (res);
+}
+
+int ft_nonbuiltin(t_cmd *cmd, char **env) {
+	pid_t pid = 0;
+	int	res = 0;
+
+	if (cmd->is_pipe)
+		if (pipe(cmd->fd) < 0)
+			exit_fatal();
+	pid = fork();
+	if (pid < 0)
+		exit_fatal();
+	else if(pid == 0) {
+		if (cmd->is_pipe && dup2(cmd->fd[1], 1) < 0)
+			exit_fatal();
+		if (cmd->is_pipe && cmd->prev && dup2(cmd->fd[0], 0) < 0)
+			exit_fatal();
+		if (execve(cmd->args[0], cmd->args, env) < 0) {
+			ft_putstr("error: cannot execute ");
+			ft_putstr(cmd->args[0]);
+			ft_putstr("\n");
+		}
+	}
+	else
+		parent(cmd, pid);
+	return (res);
+}
+
 int exec(t_cmd *cmd, char **env) {
 	int res = 0;
 
@@ -77,11 +121,12 @@ int exec(t_cmd *cmd, char **env) {
 		if (strcmp(cmd->args[0], "cd"))
 			res = ft_cd(cmd);
 		else
-			res = ft_nonbuiltin(cmd);
+			res = ft_nonbuiltin(cmd, env);
 		cmd = cmd->next;
 	}
 	return (res);
 }
+
 int		main(int ac, char **av, char **env)
 {
 	t_cmd *cmd;
