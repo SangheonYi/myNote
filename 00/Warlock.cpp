ab21857937e69cba39dbe67e363adc6684bb299a
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-03-25 10:54:09 +0900

    feat: Add 00

---
+++ 00/Warlock.cpp
@@ -0,0 +1,15 @@
+# include "Warlock.hpp"
+
+int	main() {
+	std::cout << "--- Constructors:\n";
+	Warlock richard("Richard", "Master");
+	std::cout << "--- Introduce:\n";
+	richard.introduce();
+
+	std::cout << "--- New Title:\n";
+	richard.setTitle("no mmmmas");
+	richard.introduce();
+
+	std::cout << "--- Destructors:\n"
+	return (0);
+}
