add5fa4bbb1429a9ab712fd6d7d52e5d82fc7019
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-04-07 13:07:18 +0900

    feat: 6th 02 done

---
+++ 02/BrickWall.cpp
@@ -0,0 +1,10 @@
+# include "BrickWall.hpp"
+
+BrickWall::BrickWall():
+	ATarget("BrickWall Practice") {}
+
+BrickWall::~BrickWall() {}
+		
+ATarget	*BrickWall::clone() const {
+	return (new BrickWall(*this));
+}
