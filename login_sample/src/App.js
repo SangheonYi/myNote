929095a442f6cfab275378e33c8c09127a1f1d51
tmzpdpdlwl <tmzpdpdlwl@gmail.com>
2021-04-15 12:18:01 +0900

    modified: Navigate to Message

--- login_sample/src/App.js
+++ login_sample/src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import {BrowserRouter,Route} from "react-router-dom"
-import Pay from "./routes/Pay";
+import Messege from "./routes/Messege";
 import Home from "./routes/Home";
 import Navigation from "./routes/Navigation";
-import PayResult from "./routes/PayResult";
 
 function App() {
   return <BrowserRouter>
   <Navigation/>
     <Route path="/" exact={true} component={Home}/>
-    <Route path="/Pay" component={Pay}/>
-    <Route path="/PayResult" component={PayResult}/>
+    <Route path="/Messege" component={Messege}/>
     </BrowserRouter>;
 }
 export default App;
--- login_sample/src/routes/Navigation.js
+++ login_sample/src/routes/Navigation.js
@@ -5,7 +5,7 @@ import "./Navigation.css";
 function Navigation() {
 	return <div className="nav">
 		<Link to="/">Home</Link>
-		<Link to="/Pay">Pay</Link>
+		<Link to="/Messege">Messege</Link>
 	</div>;
 }
 export default Navigation;
